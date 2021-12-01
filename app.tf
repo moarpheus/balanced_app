8353c8626dfe30b70240b0a7dbb01bf159efe7ad
JG <moarpheus@gmail.com>
2021-12-01 12:10:11 +0000

    added vpc

--- app.tf
+++ app.tf
@@ -1,15 +1,18 @@
-# vars
-# providers
-# data
-# resources
 # output
 
+# data
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# providers
 provider "aws" {
   region     = "eu-west-2"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
+# resources
 resource "aws_vpc" "main" {
   cidr_block         = var.vpc_cidr_block
   instance_tenancy   = "default"
