ec8200187fff08dce6e782967faa286b5f22a1d6
JG <moarpheus@gmail.com>
2021-12-01 11:36:29 +0000

    added separate vars file and tfvars

--- app.tf
+++ app.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block       = var.cidr_block
-  instance_tenancy = "default"
+  cidr_block         = var.vpc_cidr_block
+  instance_tenancy   = "default"
   enable_dns_support = true
 }
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+  sensitive   = true
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "Base CIDR Block for VPC"
+  default     = "10.0.0.0/16"
+}
