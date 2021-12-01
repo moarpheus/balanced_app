77da91cead4e2f3e64cc423879c47c827ec0c8da
JG <moarpheus@gmail.com>
2021-12-01 11:14:58 +0000

    added vpc

--- app.tf
+++ app.tf
@@ -9,3 +9,9 @@ provider "aws" {
   access_key = vars.aws_access_key
   secret_key = vars.aws_secret_key
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = vars.cidr_block
+  instance_tenancy = "default"
+  enable_dns_support = true
+}
